4a3800497bb7303334942047ca9b48e8c822bf52
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-08-24 23:51:36 +0100

    Update

--- src/components/add-links.js
+++ src/components/add-links.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
 
-function Add_links(props) {
+function Add_links() {
+    const [name, setName] = useState("")
+    const [link, setLink] = useState("")
+    const [description, setDescription] = useState("")
+    const handleSubmit = () => {
+        const options = {
+            url: 'https://sticky-note-me.herokuapp.com/stickynote/',
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json;charset=UTF-8'
+            },
+            data: {
+                name: name,
+                weblink: link,
+                description: description,
+            }
+        };
+
+        axios(options)
+            .then(response => {
+                alert("Success!");
+                console.log(response);
+            })
+            .catch(err => alert(err));
+    }
     return (
         <div>
             <div>
-                <Form>
+                <Form onChange={handleSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Link Name</Form.Label>
-                        <Form.Control type="text" placeholder="Link Name" />
+                        <Form.Control type="text" placeholder="Link Name" value={name} onChange={e => setName(e.target.value)} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Link</Form.Label>
-                        <Form.Control type="text" placeholder="Link" />
+                        <Form.Control type="text" placeholder="Link" value={link} onChange={e => setLink(e.target.value)} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Description</Form.Label>
-                        <Form.Control as="textarea" rows={3} />
+                        <Form.Control as="textarea" rows={3} value={description} onChange={e => setDescription(e.target.value)} />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
